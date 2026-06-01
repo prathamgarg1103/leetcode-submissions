a2477599156fa137386f98074e22d863571d63fc
Pratham Garg <pgarg7_be23@thapar.edu>
2026-06-01 20:39:00 +0530

    Time: 1941 ms (5.05%), Space: 23.9 MB (84.94%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,15 +9,11 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-
     for(ListNode* a = headA; a != NULL; a = a->next) {
-
         for(ListNode* b = headB; b != NULL; b = b->next) {
-
             if(a == b)
-                return a;
-        }
+            return a;
+            }
+        }return NULL;
     }
-    return NULL;
-}
 };
