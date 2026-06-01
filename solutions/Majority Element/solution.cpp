a6076e3cd4ba299534d470a8c730ac9565b09f11
prathamgarg1103 <209997799+prathamgarg1103@users.noreply.github.com>
2026-06-01 04:07:02 +0000

    Auto-sync LeetCode submissions

---
+++ solutions/Majority Element/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int cnt=0;
+        int el;
+        for(int i=0;i<nums.size();i++){
+            if(cnt==0){
+                cnt=1;
+                el=nums[i];
+            }
+            else if(nums[i]==el){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+        int cnt2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==el)cnt2++;
+        }
+        if(cnt2>(nums.size()/2)){
+            return el;
+        }return -1;
+    }
+};
