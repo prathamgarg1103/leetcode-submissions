9482920cb2656cf9a42e82cf3c8c97c8a8bfdd83
prathamgarg1103 <209997799+prathamgarg1103@users.noreply.github.com>
2026-06-01 15:44:27 +0000

    Auto-sync LeetCode submissions

---
+++ solutions/Intersection of Two Linked Lists/solution.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    for(ListNode* a = headA; a != NULL; a = a->next) {
+        for(ListNode* b = headB; b != NULL; b = b->next) {
+            if(a == b)
+            return a;
+            }
+        }return NULL;
+    }
+};
---
+++ solutions/Palindrome Linked List/solution.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        // Step 1: Find middle using slow/fast
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Step 2: Reverse second half
+        ListNode* second = reverse(slow);
+
+        // Step 3: Compare both halves
+        ListNode* first = head;
+        ListNode* temp = second;       // save to restore later
+        bool result = true;
+        while (second) {
+            if (first->val != second->val) {
+                result = false;
+                break;
+            }
+            first = first->next;
+            second = second->next;
+        }
+
+        return result;
+    }
+
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* temp = head;
+        while (temp) {
+            ListNode* next = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = next;
+        }
+        return prev;
+    }
+};
