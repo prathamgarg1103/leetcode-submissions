4601b2b22cebefd52fe27e483e241ca511a494ce
Pratham Garg <pgarg7_be23@thapar.edu>
2026-06-05 12:17:52 +0530

    Create 44-WildcardMatching

---
+++ 44-WildcardMatching/44-WildcardMatching.cpp
@@ -0,0 +1,30 @@
+// Last updated: 6/5/2026, 12:17:52 PM
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size(), n = p.size();
+
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        dp[0][0] = true;
+
+        // Handle patterns like *, **, ***
+        for (int j = 1; j <= n; j++) {
+            if (p[j - 1] == '*')
+                dp[0][j] = dp[0][j - 1];
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (p[j - 1] == '?' || p[j - 1] == s[i - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
