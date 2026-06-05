6aee9847e4ec27c8794bcbc1bd94d3a8d8dfbb8e
Pratham Garg <pgarg7_be23@thapar.edu>
2026-06-05 12:18:22 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.cpp
@@ -0,0 +1,13 @@
+// Last updated: 6/5/2026, 12:18:22 PM
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    return {i,j};
+                }
+            }
+        }return {};
+    }
+};
