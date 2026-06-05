f6d4c9767bacc2e10e3d37f30d6381ae05d85168
Pratham Garg <pgarg7_be23@thapar.edu>
2026-06-05 12:17:01 +0530

    Create 162-FindPeakElement

---
+++ 162-FindPeakElement/162-FindPeakElement.cpp
@@ -0,0 +1,36 @@
+// Last updated: 6/5/2026, 12:17:00 PM
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+
+        int n = nums.size();
+
+        if(n == 1) return 0;
+
+        for(int i = 0; i < n; i++) {
+
+            if(i == 0) {
+
+                if(nums[i] > nums[i+1]) {
+                    return i;
+                }
+            }
+
+            else if(i == n-1) {
+
+                if(nums[i] > nums[i-1]) {
+                    return i;
+                }
+            }
+
+            else {
+
+                if(nums[i] > nums[i-1] && nums[i] > nums[i+1]) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
