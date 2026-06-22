ff14aa7eb110ed9a5e71455c6f08db90fec9760e
Pratham Garg <pgarg7_be23@thapar.edu>
2026-06-22 14:32:52 +0530

    Time: 3 ms (12.33%), Space: 9.3 MB (15.37%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxNumberOfBalloons(string text) {
         unordered_map<char,int> mp;
-        for(char c : text) {
+        for(auto c : text) {
             mp[c]++;
         }
         return min({mp['b'],mp['a'],mp['l']/2,mp['o']/2,mp['n']});
