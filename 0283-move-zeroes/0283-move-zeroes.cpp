d5391da19748ef0e43d71138c0eb38f9f9b94615
Pratham Garg <pgarg7_be23@thapar.edu>
2026-07-05 17:35:39 +0530

    Time: 3 ms (14.66%), Space: 23.9 MB (55.79%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int i=0;
-        for(int j=0;j<nums.size();j++){
-            if(nums[j]!=0){
-                swap(nums[i],nums[j]);
+        for(int k=0;k<nums.size();k++){
+            if(nums[k]!=0){
+                swap(nums[i],nums[k]);
                 i++;
             }
         }
