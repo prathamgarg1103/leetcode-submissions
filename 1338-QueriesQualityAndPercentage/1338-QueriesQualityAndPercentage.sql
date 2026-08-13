bf662e467e8680309bd2ad44937cd74d8865b649
Pratham Garg <pgarg7_be23@thapar.edu>
2026-08-13 11:14:32 +0530

    Create 1338-QueriesQualityAndPercentage

---
+++ 1338-QueriesQualityAndPercentage/1338-QueriesQualityAndPercentage.sql
@@ -0,0 +1,10 @@
+-- Last updated: 8/13/2026, 11:14:28 AM
+SELECT
+    query_name,
+    ROUND(SUM(rating / position) / COUNT(*), 2) AS quality,
+    ROUND(
+        SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100 / COUNT(*),
+        2
+    ) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
