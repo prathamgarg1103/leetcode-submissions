e56b26020dbe39484a6cdd5d5b35146ab8f76266
Pratham Garg <pgarg7_be23@thapar.edu>
2026-08-13 11:15:52 +0530

    Create 577-EmployeeBonus

---
+++ 577-EmployeeBonus/577-EmployeeBonus.sql
@@ -0,0 +1,7 @@
+-- Last updated: 8/13/2026, 11:15:48 AM
+# Write your MySQL query statement below
+SELECT E.name,B.bonus
+FROM Employee E LEFT JOIN Bonus B
+ON E.empID=B.empID
+WHERE B.bonus < 1000
+   OR B.bonus IS NULL;
